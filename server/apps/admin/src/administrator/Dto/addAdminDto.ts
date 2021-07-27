9b67e17d1affc54918d53cbb59e7fc9deccef7c9
xuanyu <969718197@qq.com>
2021-07-27 23:10:52 +0800

    添加class-validator验证

--- server/apps/admin/src/administrator/Dto/addAdminDto.ts
+++ server/apps/admin/src/administrator/Dto/addAdminDto.ts
@@ -1,10 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-
+import { IsNotEmpty, IsString, IsEmail } from 'class-validator';
 export class addAdminDto {
   @ApiProperty({ title: '管理员名称' })
+  @IsString({ message: '用户名必须为字符类型' })
+  @IsNotEmpty({ message: '用户名不能为空' })
   name: string;
 
   @ApiProperty({ title: '邮箱' })
+  @IsString({ message: '邮箱不能为空' })
+  @IsEmail({ ignore_max_length: true }, { message: '请输入正确的邮箱' })
   email: string;
 
   @ApiProperty({ title: '密码' })
